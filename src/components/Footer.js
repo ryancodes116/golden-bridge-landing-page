1ef2ab1745f0e12a936a818ab048a648a7cc6a78
Ryan Moulton <ryanmoulton116@gmail.com>
2020-06-10 13:00:07 -0700

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,46 @@ import React from 'react';
 import classNames from 'classnames';
 import styles from '../styles/Footer.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {
+  faMapMarkerAlt,
+  faPhoneAlt,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
       <div className={classNames('container', styles.footerContent)}>
-        <div className={styles.text}>
-          <h2 className={styles.logo}>Golden Coast</h2>
+        <h2 className={styles.logo}>Golden Coast</h2>
+        <div className={styles.footerContact}>
+          <div className={styles.contactMethods}>
+            <div className={styles.contact}>
+              <FontAwesomeIcon
+                icon={faMapMarkerAlt}
+                className={styles.contactIcon}
+                id={styles.locationIcon}
+              />
+              <p className={styles.contactText}>
+                Lorem ipsum dolor sit amet, consecetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua
+              </p>
+            </div>
+            <div className={styles.contact}>
+              <FontAwesomeIcon
+                icon={faPhoneAlt}
+                className={styles.contactIcon}
+              />
+              <p className={styles.contactText}>(714) 837-7489</p>
+            </div>
+            <div className={styles.contact}>
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                className={styles.contactIcon}
+              />
+              <p className={styles.contactText}>golcen@yahoo.com</p>
+            </div>
+          </div>
+
           <a
             className={classNames('btn', styles.btn)}
             href="mailto:golcen@yahoo.com"
